0924e956dbf865b32a6fd5c20f3466e38da35418
Ynjxsjmh <ynjxsjmh@gmail.com>
2020-11-15 14:17:06 +0800

    Add meta info about imelpa

---
+++ imelpa.el
@@ -0,0 +1,31 @@
+;;; imelpa.el --- Emacs package manager -*- lexical-binding: t -*-
+
+;; Author: Ynjxsjmh
+;; Maintainer: Ynjxsjmh
+;; Version: 1.0
+;; Package-Requires: (emacs "26.3")
+;; Homepage: https://github.com/Ynjxsjmh/imelpa
+;; Keywords: package-manager
+
+
+;; This file is not part of GNU Emacs
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; For a full copy of the GNU General Public License
+;; see <http://www.gnu.org/licenses/>.
+
+
+;;; Commentary:
+;; `imelpa' can help you to manage those packages don't exist on elpa/melpa.
+
+;;; Code:
+
